c0f800a767306dbfd1a525384206dd3b98e21030
Henrique da Silva Lisboa 22C <henrique6@eaportal.org>
2024-11-13 21:03:31 +0000

    alterações no css

--- EDUTEC-2024-22C-main/script.js
+++ EDUTEC-2024-22C-main/script.js
@@ -1,17 +1,20 @@
-
 if (localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.add('dark-mode');
     document.getElementById('logo').src = './imagens/logo_modo_noturno.svg';
+    document.querySelector('.login').src = './imagens/login_modo_noturno.png';
 }
 
 document.getElementById('theme-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     const logo = document.getElementById('logo');
+    const loginButton = document.querySelector('.login');
     if (document.body.classList.contains('dark-mode')) {
         logo.src = './imagens/logo_modo_noturno.svg';
+        loginButton.src = './imagens/login_modo_noturno.png';
         localStorage.setItem('dark-mode', 'true');
     } else {
         logo.src = './imagens/logo.svg';
+        loginButton.src = './imagens/login.png';
         localStorage.setItem('dark-mode', 'false');
     }
 });
